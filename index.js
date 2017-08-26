2e8e7bc25a2363923ac6f4e69e3f0ef57ddac910
Tim Samshuijzen <timsamshuijzen@gmail.com>
2017-08-26 15:38:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/**
+ * Simple proxy server for IOTA.
+ * Relays commands to the IOTA Tangle, but intercepts attachToTangle commands and performs PoW locally.
+ * 
+ * This proxy server useful for when you want to perform transfers with iota.lib.js in Node but do not have access to a full node that grants you to perform the PoW.
+ */
+
+var iotaProxy = require('./lib/iotaproxy.js');
+
+iotaProxy.start(
+  {
+    host: 'http://iota.bitfinex.com', 
+    port: 80, 
+    localPort: 14265,
+    overrideAttachToTangle: true
+  }
+);
+
