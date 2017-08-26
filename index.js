d65a04caf5deff813d17786d7ba5cffe9b8173aa
Tim Samshuijzen <timsamshuijzen@gmail.com>
2017-08-26 16:15:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  * Simple proxy server for IOTA.
  * Relays commands to the IOTA Tangle, but intercepts attachToTangle commands and performs PoW locally.
  * 
- * This proxy server useful for when you want to perform transfers with iota.lib.js in Node but do not have access to a full node that grants you to perform the PoW.
+ * This proxy server useful for when you want to perform transfers with iota.lib.js in Node but do not 
+ * have access to a full node that grants you access to the necessary attachToTangle commands.
  */
 
 var iotaProxy = require('./lib/iotaproxy.js');
